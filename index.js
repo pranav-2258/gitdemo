e004f40565d04156931484b965401a7b90090f28
Pranav <pranav2258.be23@chitkara.edu.in>
2025-11-24 14:55:19 +0530

    versions

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+});
+
+
+app.use(express.json());app.use(express.urlencoded({ extended: true }));
+mongoose.connect("mongod://127.0.0.1:27017/test ")
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+
+app.listen(5565,()=>{
+    console.log('Server is running on port 5565');
+});
