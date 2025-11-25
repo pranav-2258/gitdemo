57a42ac6c395985713dd3ec5efbd99c952cb486e
Pranav <pranav2258.be23@chitkara.edu.in>
2025-11-25 14:52:32 +0530

    added user routes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
-
+app.use("/api/blogs",require('./routes/blog.router'));
 
 app.use(express.json());app.use(express.urlencoded({ extended: true }));
 mongoose.connect("mongod://127.0.0.1:27017/test ")
---
+++ routes/blog.router.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+router.get("/getblogs",(req,res)=>{
+    res.json({
+        success:true,
+        message:"Blogs fetched successfully",
+        blogs:[]
+    });
+});module.exports = router;
+    
