9efa06ed07347bdfdaa84bd1815a34f59de0f955
Pranav <pranav2258.be23@chitkara.edu.in>
2025-11-25 14:55:45 +0530

    minor changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 app.use("/api/blogs",require('./routes/blog.router'));
+app.use("/api/users",require('./routes/user.router'));
 
 app.use(express.json());app.use(express.urlencoded({ extended: true }));
 mongoose.connect("mongod://127.0.0.1:27017/test ")
